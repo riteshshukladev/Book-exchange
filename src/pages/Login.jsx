8fac272c07d9ddd1d78e9b5b97bc3518fc6ade8c
RItesh <shuklaritesh5678@gmail.com>
2024-08-24 04:37:39 +0530

    Directory structure for the Login/signup page

---
+++ src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import LoginForm from '../components/auth/LoginAuth'
+const Login = () => {
+  return (
+      <div>
+          <div>
+              <LoginForm/>
+          </div>
+    </div>
+  )
+}
+
+export default Login
---
+++ src/pages/Signup.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SignUpForm from '../components/auth/SignUpForm'
+
+const Signup = () => {
+  return (
+      <div>
+          <div>
+              <SignUpForm/>
+          </div>
+    </div>
+  )
+}
+
+export default Signup;
