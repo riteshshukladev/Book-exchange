a3a3a423bea3e1a474370fe0bd99043cc757be51
RItesh <shuklaritesh5678@gmail.com>
2024-08-27 20:02:27 +0530

    Done with the DeleteBookModal component, set to delete the Book listed by users

--- src/components/modals/book-manipulation-modals/DeleteBookModal.jsx
+++ src/components/modals/book-manipulation-modals/DeleteBookModal.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import {
+    Dialog,
+    DialogContent,
+    DialogHeader,
+    DialogTitle,
+    DialogFooter,
+  } from "@/components/ui/dialog";
+  import { Input } from "@/components/ui/input";
+  import { Label } from "@/components/ui/label";
+  import { Button } from "@/components/ui/button";
+  
+  import { useBookList } from "../../../store/bookListingStore";
+import BookListing from "@/components/user-pages/BookListing";
+
+//   Add the function to delete the API
 
 const DeleteBookModal = () => {
-  return (
-    <div>DeleteBookModal</div>
-  )
-}
+  const { isDeleteModalOpen, closeDeleteModal, currentBook } = BookListing();
 
-export default DeleteBookModal
+  if (!currentBook) return null;
+
+  return (
+    <Dialog open={isDeleteModalOpen} onOpenChange={closeDeleteModal}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Delete Book</DialogTitle>
+        </DialogHeader>
+        <p>Are you sure you want to delete "{currentBook.title}" by {currentBook.author}?</p>
+        <DialogFooter>
+          <Button variant="outline" onClick={closeDeleteModal}>Cancel</Button>
+          <Button variant="destructive" onClick={''}>Delete</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default DeleteBookModal;
